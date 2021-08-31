81c15d8629ec7f260e62ea43e770484c27c77b46
ankush <ankushalg@gmail.com>
2021-08-31 11:23:41 +0530

    Implemented Ad Mob Rewards Ads

--- app/src/main/java/com/allstudio/adstest/RewardingAMActivity.java
+++ app/src/main/java/com/allstudio/adstest/RewardingAMActivity.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.ads.rewarded.RewardedAd;
+
 public class RewardingAMActivity extends AppCompatActivity {
+    private RewardedAd mRewardedAd;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
